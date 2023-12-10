9ce49f726818add442c27295943d1a13bb5c43d9
Yaejin Kim <71435571+ggjiny@users.noreply.github.com>
2023-12-11 01:43:54 +0900

    Delete js directory

--- js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["lake.jpg", "sunrise.jpg", "milkyway.jpg", "astronomy.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-//const bgImage = document.createElement("img");
-document.body.style.backgroundImage = `url(img/${chosenImage})`;
-//bgImage.src = `img/${chosenImage}`;
-
-//document.body.appendChild(bgImage);
